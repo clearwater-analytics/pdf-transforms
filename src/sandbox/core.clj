a7a44d351cfccd98cef7592ee9615fa4ab67f9f8
Tshort76 <thomaslong@u.boisestate.edu>
2018-03-16 15:15:48 -0600

    add code for block composition analysis

--- src/sandbox/core.clj
+++ src/sandbox/core.clj
@@ -7,7 +7,9 @@
             [pdf-transforms.common :as cmn]
             [pdf-transforms.core :as core]
             [sandbox.utils :as u]
-            [clojure.string :as s]))
+            [clojure.string :as s]
+            [plumbing.core :refer [fnk]]
+            [plumbing.graph :as graph]))
 
 
 ;TODO add a 'center of mass' comparison check (to test alignment difference between block and other words)
@@ -18,7 +20,6 @@
                            (and (or
                                   (:horizontal-bar? (first stream))
                                   (re-matches utils/dash-line (:text (first stream)))
-                                  (re-matches #".*(?:[:]|[.]{2,})" (:text (peek block))) ;added
                                   (b/y-gap? block (first stream)))
                                 (not (b/x-gap? block stream))))
             :irrelevant? (fn [block stream]
@@ -37,6 +38,41 @@
        parse-blocks
        (a/annotate {:pdf-url pdf-path :output-directory (or out u/annotated-dir)})
        dorun))
+
+(def line-metrics-graph
+  {:lines           (fnk [content] (utils/create-lines content))
+   :pretty-text     (fnk [lines] (s/join "\n" (map (comp (partial s/join " ") (partial map :text)) lines)))
+   :spacings        (fnk [lines] (if (= 1 (count lines))
+                                   []
+                                   (->> lines
+                                        (map (comp first (partial remove :superscript?)))
+                                        (partition 2 1)
+                                        (map (fn [[{ay :y} {by :y bh :height}]] (- (+ by bh) ay))))))
+   :spacing-avg     (fnk [spacings] (when (> (count spacings) 0) (/ (reduce + spacings) (count spacings))))
+   :spacing-std-dev (fnk [spacings spacing-avg]
+                      (when spacing-avg
+                        (Math/sqrt (/ (reduce (fn [sum x]
+                                                (+ sum (Math/pow (- spacing-avg x) 2.0))) 0 spacings)
+                                      (count spacings)))))})
+
+(def line-metrics (graph/compile line-metrics-graph))
+
+
+(defn font-metrics [{:keys [content]}]
+  {:font-sizes (into #{} (keep :f-size content))
+   :size-mode  (ffirst (max-key second (frequencies (keep :f-size content))))
+   :fonts      (into #{} (keep (comp b/font-clean :font) content))})
+
+(defn add-metrics [block]
+  (merge (line-metrics block) (font-metrics block) block))
+
+(defn block-analysis [pdf-url]
+  (->> pdf-url
+       parse-blocks
+       (a/annotate {:pdf-url pdf-url :output-directory u/annotated-dir})
+       (map (comp #(dissoc % :lines :content) add-metrics)))
+
+  )
 
 #_(annotate-blocks (str "file:" u/home-dir "/temp.pdf"))
 
@@ -58,15 +94,15 @@
 
 ;;;;;;;;;;;; OLDER STUFF  ;;;;;;;;;;
 
-(def delimiter-ending #".*(?:[.]{2,}|\-+|\:)")
+(defn new-segment? [{ax :x :as a} {bx :x :as b}]
+  (or (> ax bx) (utils/gap? a b) (utils/new-line? a b)))
 
-(defn new-segment? [{ax :x ap :page-number :as a}
-                    {bx :x bp :page-number :as b}]
-  (or
-    (> ax bx)
-    (utils/gap? a b)
-    (utils/new-line? a b)
-    (not= ap bp)))
+(defn annotate-segments [pdf-url]
+  (->> (pe/extract-char-positions pdf-url)
+       pd/->pages-of-words
+       (mapcat (partial utils/partition-when new-segment?))
+       (map cmn/boundaries-of)
+       (a/annotate {:pdf-url pdf-url :output-directory u/annotated-dir})))
 
 
 
@@ -77,11 +113,5 @@
 
 #_(->> u/dir
        u/get-pdfs-in-dir
-       (map #(->> (pe/extract-char-positions %)
-                  pd/->pages-of-words
-                  (apply concat)
-                  (utils/partition-when new-segment?)
-                  (map (comp (fn [x] (assoc x :class :table)) cmn/boundaries-of))
-                  (a/annotate {:pdf-url % :output-directory u/annotated-dir})
-                  dorun))
+       (map annotate-segments)
        dorun)
