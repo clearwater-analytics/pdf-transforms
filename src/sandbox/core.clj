ed94941d0266ea1f08906b44ea1b96cab3cec839
TShort76 <thomaslong@u.boisestate.edu>
2018-04-21 10:13:52 -0600

    refine block composition logic

--- src/sandbox/core.clj
+++ src/sandbox/core.clj
@@ -12,24 +12,66 @@
             [plumbing.graph :as graph]))
 
 
+;TODO better gap logic
+; End of sentence
+;    bob." hello
+;    JON. SALLY
+;    today. Co-owner
+;    purposes. Any
+;    borrowers.  See,
+
+; Footnotes, superscripts, bullets, Itemized lines all need a larger gap threshold
+
+;Mid sentence gaps
+; yment related defaults, if mate financial difficulties; (iv) difficulties; (v) substitution of
+; Council; former Board member and past President Association of Counties; past President,
+;  Missouri-Columbia (B.S., Business
+
+
+
+;TODO increase acceptable gap size for footnotes, bullets, (VI) xxx
+
+
+
+
 ;;;;;;;;;;;; SEGMENTS  ;;;;;;;;;;
 
 (def segment-decor-graph
-  {:text      (fnk [tokens] (s/join " " (map :text tokens)))
-   ;:mid-x     (fnk [x0 x1] (/ (+ x1 x0) 2.0))
-   ;:width     (fnk [x0 x1] (- x1 x0))
-   :height    (fnk [y0 y1] (- y1 y0))
-   :alignment (fnk [x0 x1] nil)
-   :font      (fnk [tokens] (->> tokens (map (comp b/font-clean :font)) frequencies (apply max-key second) first))
-   :font-size (fnk [tokens] (->> tokens (map :font-size) frequencies (apply max-key second) first))
-   })
+  {:text                 (fnk [tokens] (s/join " " (map :text tokens)))
+   ;:width-ratio          (fnk [page-bounds x0 x1]
+   ;                        (/ (- x1 x0)
+   ;                           (- (:x1 page-bounds)
+   ;                              (:x0 page-bounds))))
+   ;:width-class          (fnk [width-ratio]
+   ;                        (cond
+   ;                          ;(>= width-ratio 0.95) 4
+   ;                          (>= width-ratio 0.75) 3
+   ;                          (>= width-ratio 0.5) 2
+   ;                          (>= width-ratio 0.25) 1
+   ;                          :else 0))
+   ;:horizontal-alignment (fnk [x0 x1 page-bounds width-class]
+   ;                        (let [{px0 :x0 px1 :x1} page-bounds]
+   ;                          (cond
+   ;                            (or
+   ;                              (utils/within-x? 10 x0 px0)
+   ;                              (and (utils/within-x? 50 x0 px0)
+   ;                                   (> (- (- x0 px0) (- px1 x1)) 10)
+   ;                                   (> width-class 2))) :left
+   ;                            (and
+   ;                              (> (- px1 x1) 50)
+   ;                              (utils/within-x? 10 (- px1 x1) (- x0 px0))) :center
+   ;                            (utils/within-x? 15 x1 px1) :right
+   ;                            :else :float)))
+   :height               (fnk [y0 y1] (- y1 y0))
+   :font                 (fnk [tokens] (->> tokens (map (comp b/font-clean :font)) frequencies (apply max-key second) first))
+   :font-size            (fnk [tokens] (->> tokens (map :font-size) frequencies (apply max-key second) first))})
 
 (def decorate (graph/compile segment-decor-graph))
 
-(defn decorate-segment [tokens]
+(defn decorate-segment [page-boundaries tokens]
   (-> tokens
       cmn/boundaries-of
-      (assoc :tokens tokens)
+      (assoc :tokens tokens :page-bounds page-boundaries)
       ((juxt identity decorate))
       ((partial apply merge))))
 
@@ -40,68 +82,33 @@
                      1.0)
                    (min height b-height))))
 
+;TODO can increase performance by short-circuiting ...
+(defn filter-candidates [segment other-segments blocks]
+  (:candidates
+    (reduce (fn [{:keys [candidates] :as box} seg]
+              (if (and (= #{:below} (cmn/relative-to box seg))
+                       (vertically-near? (last candidates) seg))
+                (let [{cands :candidates :as hypothesis} (-> box (utils/expand-bounds seg) (update :candidates conj seg))]
+                  (if (some (fn [seg-x] (and (empty? (cmn/relative-to hypothesis seg-x)) ;within block
+                                             (not (some (partial = seg-x) cands))))
+                            (concat other-segments blocks))
+                    (reduced box)
+                    hypothesis))
+                box))
+            (assoc segment :candidates [segment])
+            other-segments)))
 
-(def test-data [{:font-size 9.0,
-                 :y1        224.94,
-                 :width     68.49008178710938,
-                 :font      "BowneKensingtonBold",
-                 :class     :generic,
-                 :x1        331.9900817871094,
-                 :x0        263.5,
-                 :y0        220.40399974822998,
-                 :height    4.5360002517700195,
-                 :text      "TITLE"}
-                {:font-size 9.0,
-                 :y1        236.44,
-                 :width     272.78997802734375,
-                 :x1        536.2899780273438,
-                 :x0        263.5,
-                 :font      "BowneKensingtonRoman",
-                 :class     :generic,
-                 :y0        232.0975002670288,
-                 :height    4.342499732971191,
-                 :text      "PARAGRAPH"}
-                {:font-size 9.0,
-                 :y1        245.94,
-                 :x1        513.7898559570312,
-                 :x0        263.5,
-                 :width     250.28985595703125,
-                 :font      "BowneKensingtonRoman",
-                 :class     :generic,
-                 :y0        241.5975002670288,
-                 :height    4.342499732971191,
-                 :text      "PARAGRAPH"}
-                {:font-size 9.0,
-                 :y1        255.44,
-                 :width     215.18975830078125,
-                 :font      "BowneKensingtonRoman",
-                 :class     :generic,
-                 :y0        251.0975002670288,
-                 :x1        478.68975830078125,
-                 :x0        263.5,
-                 :height    4.342499732971191,
-                 :text      "PARAGRAPH"}
-                {:font-size 9.0,
-                 :y1        269.94,
-                 :width     131.13015747070312,
-                 :font      "BowneKensingtonBold",
-                 :class     :generic,
-                 :y0        265.40399974823,
-                 :x1        394.6301574707031,
-                 :x0        263.5,
-                 :height    4.5360002517700195,
-                 :text      "TITLE 2"}])
 
-(defn group-vertically [block other-blocks]
+
+
+
+(defn group-vertically [segment other-segments other-blocks]
   (let [calc-diff #(- (:y0 (second %)) (:y1 (first %)))
         pairs (->> other-blocks
-                   (filter #(= #{:below} (cmn/relative-to block %)))
-                   (concat [block])
-                   (sort-by :y0)
-                   (partition 2 1)
-                   (take-while (partial apply vertically-near?)))]
+                   (filter-candidates segment other-segments)
+                   (partition 2 1))]
     (case (count pairs)
-      0 [block]
+      0 [segment]
       1 (first pairs)
       (:block (reduce (fn [{:keys [last-diff block]} pair]  ;group by magnitude of separation
                         (let [diff (calc-diff pair)]
@@ -126,21 +133,21 @@
 
 ;TODO can improve 'remove' performance later if necessary
 (defn build-blocks [segments]
-  (map format-block
-       (loop [blocks []
-              remaining segments]
-         (if-not (seq remaining)
-           blocks
-           (let [block (group-vertically (first remaining) (rest remaining))]
-             (recur (conj blocks block) (remove (into #{} block) remaining)))))))
+  (loop [blocks []
+         remaining segments]
+    (if-not (seq remaining)
+      blocks
+      (let [block (group-vertically (first remaining) (rest remaining) blocks)]
+        (recur (conj blocks (format-block block)) (remove (into #{} block) remaining))))))
 
 (def levels [:tokens :segments :blocks :components])
 
 (defn parse-page [page-of-tokens & [{:keys [level] :or {level :components}}]]
-  (let [lvl (.indexOf levels level)]
+  (let [lvl (.indexOf levels level)
+        page-bounds (cmn/boundaries-of page-of-tokens)]
     (cond->> page-of-tokens
              (> lvl 0) (utils/partition-when b/new-segment?)
-             (> lvl 0) (map decorate-segment)
+             (> lvl 0) (map (partial decorate-segment page-bounds))
              (> lvl 1) build-blocks)))
 
 (defn annotate-em [pdf-url & [{:keys [out] :as opts}]]
@@ -149,15 +156,20 @@
        (mapcat #(parse-page % opts))
        (a/annotate {:pdf-url pdf-url :output-directory (or out u/annotated-dir)})))
 
-#_(let [base-dir (str u/home-dir "/Documents/pdf_parsing/control_2/")]
+#_(->> {:level :segments}
+       (annotate-em (str "file:" u/home-dir "/Documents/pdf_parsing/control_2/raw/transposed_table.pdf"))
+       (map #(dissoc % :tokens))
+       )
+
+#_(let [base-dir (str u/home-dir "/Documents/pdf_parsing/control_3/")]
     (->> (str base-dir "raw")
          u/get-pdfs-in-dir
-         (map #(annotate-em % {:out (str base-dir "segments") :lvl :segments}))
+         (map #(annotate-em % {:out (str base-dir "segments") :level :segments}))
          dorun))
 
 #_(let [base-dir (str u/home-dir "/Documents/pdf_parsing/control_2/")]
     (->> (str base-dir "raw")
          u/get-pdfs-in-dir
          (map #(do (println "processing: " %)
-                   (annotate-em % {:out (str base-dir "blocks") :lvl :blocks})))
+                   (annotate-em % {:out (str base-dir "blocks") :level :blocks})))
          dorun))
